456b12f2b40d32f83097787480f2b3ee4af69a00
Ildrassil <schneiderjakob7@googlemail.com>
2023-12-04 11:55:33 +0100

    Porject_foundation

--- src/main/java/PasswordValidierung.java
+++ src/main/java/PasswordValidierung.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class PasswordValidierung {
 
-    public boolean isPassword(char[] password, int m, int n){
+    public static boolean isPassword(char[] password, int m, int n){
 
-        if( password.length == 0 || password.length < n || password.length > m){
+        if( password.length == 0 || password.length > n || password.length < m){
             return false;
         }
+        String passwordUsualCheck = password.toString();
+
 
         boolean upper = false;
         boolean lower = false;
@@ -23,15 +27,76 @@ public class PasswordValidierung {
                 digit = true;
                 numberCount++;
                 
-            } else if (!(Character.isLetter(ch))) {
-                if (!(Character.isDigit(ch))) {
+            } else if (!(Character.isLetter(ch)) && !(Character.isDigit(ch))) {
+
 
 
                     symbol = true;
-                }
+
             }
-            return upper && lower && digit && symbol && numberCount >= 4;
+
         }
+        return upper && lower && digit && symbol && numberCount >= 4;
+    }
+
+    public static boolean passwordCheck(String password, String acutualPassword){
+        if (password == acutualPassword){
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public static boolean password(String actualPassword, String inputPassword){
+        char[] actualPasswordCharArray = actualPassword.toCharArray();
+        boolean isActualPasswordAPassword = isPassword(actualPasswordCharArray, 8,99);
+        boolean passwordIsValid = false;
+        if(isActualPasswordAPassword){
+           passwordIsValid = passwordCheck(actualPassword, inputPassword);
+        }
+        return passwordIsValid;
+
+
 
     }
+    public static void CheckPassword()
+    {
+        String[] commonPass =  {"1234", "testing", "dragon", "passowrd","abc","ABC","Abc","aBc","ABc","abC","aBC", "9876", "qwerty", "football","111111","1111","letmein","trustnot","princess","Iloveu","Iloveyou","iloveyou","sunshine","Sunshine","adobe123","123456789"};
+
+        System.out.println("Requirements for the Password:");
+        System.out.println("1. It must contain at least one Capital Letter.");
+        System.out.println("2. It must contain at least 4 digits.");
+        System.out.println("3. It must contain at least one Symbol.");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter your password.");
+        String password = s.nextLine();
+        char[] passwordArray = password.toCharArray();
+        boolean validPassword = isPassword(passwordArray, 8, 99);
+        if(validPassword == true) {
+            for (int i = 0; i < commonPass.length; i++) {
+                if (password.contains(commonPass[i])) {
+                    System.out.println("Your Passowrd is too Weak. Please read the Requirements again.");
+                    CheckPassword();
+                }
+
+            }
+        }
+
+        else{
+            System.out.println("Your Passowrd is too Weak. Please read the Requirements again.");
+            CheckPassword();
+        }
+        System.out.println("Your Password is Strong!");
+
+    }
+
+    public static void main(String[] args) {
+        CheckPassword();
+        System.out.println("was Sucessful!");
+    }
+
+
+
 }
--- src/test/java/PasswordValidierungTest.java
+++ src/test/java/PasswordValidierungTest.java
@@ -1,5 +1,27 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidierungTest {
+
+    @Test
+    void PasswordValidierung(){
+        String inputPassowrd = "Testpasswort8354!";
+        String actualPassowrd = "Testpasswort8354!";
+        boolean isValid = PasswordValidierung.password(inputPassowrd,actualPassowrd);
+        boolean expected = true;
+        assertEquals( expected, isValid);
+
+    }
+
+    @Test
+    void PasswordVlaid(){
+        String input = "Testpasswort8354!";
+        char [] inputPassword = input.toCharArray();
+        boolean isValid = PasswordValidierung.isPassword(inputPassword, 8, 99);
+        boolean ecpected = true;
+
+        assertEquals(ecpected,isValid);
+    }
 
 }
